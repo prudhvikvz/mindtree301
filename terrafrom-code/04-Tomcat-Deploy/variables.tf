795250345c1d1dd865a358b9efd140aa983950af
root <root@vmadminwer.ahzrw0ydr54uhkftl3biffh4hb.cx.internal.cloudapp.net>
2020-05-05 10:38:14 +0000

    var.tf

--- terrafrom-code/04-Tomcat-Deploy/variables.tf
+++ terrafrom-code/04-Tomcat-Deploy/variables.tf
@@ -1,9 +1,9 @@
 variable "key_name" {
-  default = "mykey-test-1"
+  default = "mykey"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/amit-personal-aws.pem"
+  default = "/root/.ssh/mykey.pem"
 }
 
 variable "us-east-zones" {
@@ -11,5 +11,5 @@ variable "us-east-zones" {
 }
 
 variable "sg-id" {
-  default = "sg-0eec038018ea2902d"
+  default = "sg-06174e7de474905a1"
 }
